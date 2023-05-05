007c7bed8f2bb397894c9fb4885ade3b6cc264be
KoiParadise <i@himoyo.cn>
2023-05-05 18:32:49 +0800

    add fingerprint

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,7 @@ import {
     FaChevronRight,
     FaList,
     FaBlog,
+    FaThList,
 } from 'react-icons/fa'
 import React from 'react'
 import { IoBulb, IoLanguage, IoSchool } from 'react-icons/io5'
@@ -54,6 +55,10 @@ export default () => {
                     <LabelItem>Git</LabelItem>
                 </LabelGroup>
             </div>
+            <div>
+                <LabelGroup icon={FaThList} title="Fingerprint">
+                    <LabelItem>A0DB 23CE E377 AF83 06E6  21D9 8636 C00F 8EAD 0ECD</LabelItem>
+                </div>
         </Header>
         <Block>
         <Column>
